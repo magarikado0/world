3a707563919cf51b93e3df8f4e5904bf08245d2e
“magarikado <ka5ryouma@gmail.com>
2020-10-22 21:20:39 +0900

    all_human to human, all_animal to animal

--- world.py
+++ world.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 import time
 plt.xlim([0,100])
 plt.ylim([0,100])
-all_human = []
-all_animal = []
+human = []
+animal = []
 count = 0
 fig = plt.figure()
 for i in range(100):
-    all_human.append(mwc.human(i, random.randint(0,100), random.randint(0, 100)))
+    human.append(mwc.human(i, random.randint(0,100), random.randint(0, 100)))
 for i in range(50):
-    all_animal.append(mwc.animal(i, random.randint(0,100), random.randint(0, 100)))
+    animal.append(mwc.animal(i, random.randint(0,100), random.randint(0, 100)))
 while(count<10):
-    mwc.plot_all(all_human, all_animal)
-    for i in all_human:
+    mwc.plot_all(human, animal)
+    for i in human:
         i.migration(1, 1)
     time.sleep(0.5)
     count += 1
