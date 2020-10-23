b3043a9e61e64aef3f701110184c63a0e5800252
“magarikado <ka5ryouma@gmail.com>
2020-10-23 20:39:08 +0900

    grass

--- myworldclass.py
+++ myworldclass.py
@@ -3,13 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-class creature:
-    def __init__ (self, id, x, y):
+class existance:
+    def __init__(self, id, x, y):
         self.id = 0
         self.x = x
         self.y = y
         self.life = 1
-    def migration (self, x, y):#migration
+        self.food_want = 100
+    def migration(self, x, y):#migration
         self.x += x
         self.y += y
         if self.x > 100:
@@ -21,29 +22,44 @@ class creature:
         elif self.y < 0:
             self.y = 0
 
-class human(creature):#human
-    def __init__ (self, id, x, y):
+class human(existance):#human
+    def __init__(self, id, x, y):
         super(human, self).__init__(id, x, y)
-        self.food_want = 0
         self.relation = np.zeros(100)
         self.relation[id] = 0
     def eat (self, animal):#eat
         self.food_want -= 80
         animal.life = 0
 
-class canivore(creature):
-    def __init__ (self, id, x, y):
-        super(canivore, self).__init__(id, x, y)
+class carnivore(existance):
+    def __init__(self, id, x, y):
+        super(carnivore, self).__init__(id, x, y)
+    def eat(self, prey):
+        self.food_want -=10
+        prey.life = 0
 
-class herbivore(creature):
-    def __init__ (self, id, x, y):
+class herbivore(existance):
+    def __init__(self, id, x, y):
         super(herbivore, self).__init__(id, x, y)
+    def eat(self, grass):
+        grass.lire = 0
 
+class grass(existance):
+    def __init__(self, id, x, y):
+        super(grass, self).__init__(id, x, y)
 
-def plot_all (humanlist, animallist):
+def plot_all(human, carnivore, herbivore, grass):
     plt.cla()
-    for i in humanlist:
-        plt.scatter(i.x, i.y, s=5, c="b")
-    for i in animallist:
-        plt.scatter(i.x, i.y, s=5, c="g")
+    for i in human:
+        if i.life == 1:
+            plt.scatter(i.x, i.y, s=5, c="brown")
+    for i in carnivore:
+        if i.life == 1:
+            plt.scatter(i.x, i.y, s=5, c="red")
+    for i in herbivore:
+        if i.life == 1:
+            plt.scatter(i.x, i.y, s=5, c="blue")
+    for i in grass:
+        if i.life == 1:
+            plt.scatter(i.x, i.y, s=5, c="green")
     plt.pause(0.2)
--- world.py
+++ world.py
@@ -5,15 +5,21 @@ import time
 plt.xlim([0,100])
 plt.ylim([0,100])
 human = []
+carnivore= []
 herbivore = []
+grass = []
 count = 0
 fig = plt.figure()
 for i in range(100):
     human.append(mwc.human(i, random.randint(0,100), random.randint(0, 100)))
 for i in range(50):
+    carnivore.append(mwc.carnivore(i, random.randint(0,100), random.randint(0, 100)))
+for i in range(50):
     herbivore.append(mwc.herbivore(i, random.randint(0,100), random.randint(0, 100)))
+for i in range(50):
+    grass.append(mwc.grass(i, random.randint(0,100), random.randint(0, 100)))
 while(count<10):
-    mwc.plot_all(human, herbivore)
+    mwc.plot_all(human, carnivore, herbivore, grass)
     for i in human:
         i.migration(1, 1)
     time.sleep(0.5)
