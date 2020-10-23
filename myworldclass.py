4f849f283bd0853fdc1f81c3ee424cdcc14cc57d
“magarikado <ka5ryouma@gmail.com>
2020-10-23 19:48:42 +0900

    superclass

--- myworldclass.py
+++ myworldclass.py
@@ -1,16 +1,14 @@
 import random
 import matplotlib.pyplot as plt
 import numpy as np
-class human:#human
+
+
+class creature:
     def __init__ (self, id, x, y):
-        self.id = id#id
-        self.food_want = 0
+        self.id = 0
         self.x = x
         self.y = y
         self.life = 1
-        self.relation = np.zeros(100)
-        self.relation[id] = 20
-        #self.energy=0
     def migration (self, x, y):#migration
         self.x += x
         self.y += y
@@ -22,15 +20,26 @@ class human:#human
             self.y = 100
         elif self.y < 0:
             self.y = 0
+
+class human(creature):#human
+    def __init__ (self, id, x, y):
+        super(human, self).__init__(id, x, y)
+        self.food_want = 0
+        self.relation = np.zeros(100)
+        self.relation[id] = 0
     def eat (self, animal):#eat
         self.food_want -= 80
         animal.life = 0
-class animal:#animal
+
+class canivore(creature):
     def __init__ (self, id, x, y):
-        self.id = id
-        self.x = x
-        self.y = y
-        self.life = 1
+        super(canivore, self).__init__(id, x, y)
+
+class herbivore(creature):
+    def __init__ (self, id, x, y):
+        super(herbivore, self).__init__(id, x, y)
+
+
 def plot_all (humanlist, animallist):
     plt.cla()
     for i in humanlist:
--- world.py
+++ world.py
@@ -5,15 +5,15 @@ import time
 plt.xlim([0,100])
 plt.ylim([0,100])
 human = []
-animal = []
+herbivore = []
 count = 0
 fig = plt.figure()
 for i in range(100):
     human.append(mwc.human(i, random.randint(0,100), random.randint(0, 100)))
 for i in range(50):
-    animal.append(mwc.animal(i, random.randint(0,100), random.randint(0, 100)))
+    herbivore.append(mwc.herbivore(i, random.randint(0,100), random.randint(0, 100)))
 while(count<10):
-    mwc.plot_all(human, animal)
+    mwc.plot_all(human, herbivore)
     for i in human:
         i.migration(1, 1)
     time.sleep(0.5)
