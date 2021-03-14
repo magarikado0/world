44a61e0010d5c9e975cfb02a93aa0fe7ce1852a4
magarikado <65599773+magarikado0@users.noreply.github.com>
2021-03-14 20:28:04 +0900

    Update world.py

--- world.py
+++ world.py
@@ -2,25 +2,20 @@ import myworldclass as mwc
 import random
 import matplotlib.pyplot as plt
 import numpy as np
-fig = plt.figure()
-plt.xlim([0,100])
-plt.ylim([0,100])
 
 human = []
 carnivore= []
 herbivore = []
 grass = []
 tree_nut = []
-dis_min = 20000
-min_index = 0
 eatcount = np.zeros(8, dtype=np.int8)#0:hc 1:hb 2:ht 3:ch 4:cb 5:ct 6:bg 7:bt
 
 count = 0
-human = mwc.setlist(human, 1)
-carnivore = mwc.setlist(carnivore, 2)
-herbivore = mwc.setlist(herbivore, 3)
-grass = mwc.setlist(grass, 4)
-tree_nut = mwc.setlist(tree_nut, 5)
+human = mwc.generate_object(human, 1)
+carnivore = mwc.generate_object(carnivore, 2)
+# herbivore = mwc.generate_object(herbivore, 3)
+# grass = mwc.generate_object(grass, 4)
+# tree_nut = mwc.generate_object(tree_nut, 5)
 
 while(count < 50):
     mwc.plot_all(human, carnivore, herbivore, grass, tree_nut)
@@ -30,9 +25,8 @@ while(count < 50):
     for i in carnivore:
         if i.life == 1:
             i.action(human, carnivore, herbivore, grass, tree_nut, eatcount)
-    
-    for i in herbivore:
-        if i.life == 1:
-            i.action(human, carnivore, herbivore, grass, tree_nut, eatcount)
+    # for i in herbivore:
+    #     if i.life == 1:
+    #         i.action(human, carnivore, herbivore, grass, tree_nut, eatcount)
     count += 1
 print(eatcount)
